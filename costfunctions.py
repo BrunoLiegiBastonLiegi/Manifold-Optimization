998588f33235e884ab2edd7bd7bbb26a2d6708fa
andrea <andreapapaluca@gmail.com>
2019-09-16 17:01:16 +0200

    first upload

---
+++ costfunctions.py
@@ -0,0 +1,98 @@
+import numpy as np
+import scipy as sp
+from theta.costfunctions import logarithmic
+
+
+
+
+
+
+
+class KullbackLeibler():
+    """Empirical  Kullback Leibler divergence.
+
+    """
+    def __init__(self, model):
+        self.model = model
+    
+    def cost(self, x, *y):
+        pdf, bins = np.histogram(x, bins='auto', density=True)
+        for i in range(bins.size-1):
+            bins[i] = 0.5*(bins[i]+bins[i+1])
+        bins = np.delete(bins,bins.size-1)    
+        pdf = pdf.reshape(1,pdf.size)    
+        bins = bins.reshape(1,bins.size)
+        return np.sum(sp.special.kl_div(pdf, self.model(bins)))
+
+
+
+
+    
+    
+
+class LogLikelyhood():
+    """Empirical  Loglikelyhood.
+
+    """
+    
+    def __init__(self, model):
+        self.model = model
+    
+    def cost(self, x, *y):
+        pdf, bins = np.histogram(x, bins='auto', density=True)
+        for i in range(bins.size-1):
+            bins[i] = 0.5*(bins[i]+bins[i+1])
+        bins = np.delete(bins,bins.size-1)    
+        pdf = pdf.reshape(1,pdf.size)    
+        bins = bins.reshape(1,bins.size)
+        return -np.sum(np.log(self.model(bins)))    
+
+
+
+
+
+
+
+
+class sparselog():
+    """Sparse Loglikelyhood.
+
+    """
+    def __init__(self, model, l=500):
+        self.model = model
+        self.l = l
+
+    def cost(self, x, *y):
+        q = self.model.q
+        w = self.model.w
+        #c1 = np.sqrt(np.sum(q**2)) # Frobenius norm
+        #c2 = np.sqrt(np.sum(w**2)) # Frobenius norm
+        c1 = np.sum(np.abs(q)) # L1 norm
+        c2 = np.sum(np.abs(w)) # L1 norm
+        return logarithmic.cost(x) + self.l*c1 + self.l*c2
+    
+
+
+
+
+
+class sparseKL():
+    """Sparse KL divergence.
+
+    """
+    def __init__(self, model, l=0.3):
+        self.model = model
+        self.l = l
+
+    def cost(self, x, *y):
+        q = self.model.q
+        w = self.model.w
+        #c1 = np.sqrt(np.sum(q**2)) # Frobenius norm
+        #c2 = np.sqrt(np.sum(w**2)) # Frobenius norm
+        c1 = np.sum(np.abs(q)) # L1 norm
+        c2 = np.sum(np.abs(w)) # L1 norm
+        kl = KullbackLeibler(self.model)
+        return kl.cost(x) + self.l*c1 + self.l*c2
+
+    
+
---
+++ manifold.py
@@ -0,0 +1,58 @@
+import numpy as np
+import scipy as sp
+
+
+
+
+
+
+
+
+
+
+class Manifold(object):
+    """Custom Manifold for an RTBM object"""
+
+    def __init__(self, model):
+
+        self.model = model
+
+    @property
+    def dim(self):
+        nh = self.model._Nh 
+        nv = self.model._Nv
+        en = nh + nv
+        pn = (nh*(nh+1) + nv*(nv+1))/2 + nh*nv 
+        return (en,pn)
+        
+    def retr(self, x, v):
+        er = x[0] + v[0]
+        tmp = np.linalg.solve(x[1], v[1])
+        pr = np.dot(x[1],sp.linalg.expm(tmp))
+        return (er, pr)
+    
+    def norm(self, x, v):
+        en = np.sum(v[0]**2)
+        invx = sp.linalg.inv(x[1])
+        tmp = np.dot(invx, np.dot(v[1], np.dot(invx, v[1])))
+        pn = np.trace(tmp)
+        #return (en, pn)
+        return np.sqrt(en + pn)     
+
+    def transp (self, x1, x2, v):
+        E = sp.linalg.sqrtm(np.dot(x2[1],np.linalg.inv(x1[1])))
+        pt = np.dot(E,np.dot(v[1],E.T))
+        return [v[0] ,pt]
+
+    def diff(self, x1, x2):      # beware: it makes sense only in the tangent space
+        return (x1[0]-x2[0], x1[1]-x2[1])
+
+    def sum(self, x1, x2):      # beware: it makes sense only in the tangent space
+        return (x1[0]+x2[0], x1[1]+x2[1])
+
+    def rgrad(self, x, egrad):
+        eg = egrad[0]
+        pg = egrad[1]
+        return (eg, 0.5*np.dot(x[1], np.dot(pg + pg.T, x[1])))
+        
+
